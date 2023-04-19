4e1c69a41ce85a5a62db205dbbd9102d29a0077f
maheshopsmx <mahesh.kota@opsmx.io>
2023-04-19 20:12:43 +0530

    added instruction and files

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-# yaml-stages
+# pipeline-json
+
+
+## Architecture (Draft)
+
+![Architecture](pics/Architecture.jpg)
+
+## Steps to be followed 
+
+1.  Clone the repo 
+
+        git clone https://github.com/OpsMx/yaml-stages.git
+    
+        cd pipeline-json
+
+2. Specify the stages in the stage-input.yml (for now it supports wait and manujudgement stages)
+
+       stages: 
+         waitstage:        #stage 1 and parameters
+            waitTime: 20    # ignore if not change
+         manualjudgement:                       # stage 2 and parameters
+            instructions: This is demo pipeline  # ignore if no change 
+
+
+3. Run the script
+
+        bash run.sh
+        
+4. Once it is ran in output it will prints the pipeline json and  save it to the spinnaker
+
+
+
+5. It can also be saved in  complete_pipeline.json
+
+6. Take the pipeline json and create spinnaker pipeline
+
+ -  **Click on create pipeline**
+ 
+
+![Createpipeline](pics/createpieline.png)
+
+
+![savepipe](pics/savepipe.png)
+
+  - **Click on pipeline action**
+
+
+![pipeaction](pics/pipeaction.png)
+
+  - **Click on edit as json**
+
+
+![editjson](pics/editjson.png)
+
+  - **Remove old json and copy paste the new json**
+
+![savepipejs](pics/savepipejs.png)
+
+  - **finally save the pipeline**
+
+![savepipen.png](pics/savepipen.png)
+ 
+  - **Now run the pipeline**
+
+![savepipejs](pics/savepipejs.png)
+
+  - **Pieline Execution will be as below**
+
+
+![runpip](pics/runpip.png)
---
+++ run.sh
@@ -0,0 +1,82 @@
+#! /bin/bash
+echo "Fetch the stages from input file ..."
+
+#stages=$(yq -r .stages stage-input.yml)
+stages=$(yq -r '.stages| to_entries | .[] | .key' stage-input.yml | xargs | sed -e 's/ /,/g')
+
+if [[ -z "$stages" ]]; then
+  echo "ERROR: Please specify the stages in the input file....."
+  exit 1
+else 
+  git clone https://github.com/OpsMx/yaml-stages.git 2> /tmp/tmp.log
+  cd pipeline-json/stages
+  rm -rf ../list
+  touch ../list
+  IFS=","
+  for liststages in $stages
+  do
+    existone=waitstage,manualjudgement,deploymanifest
+    loopcount=$(echo $existone | tr ',' ' ' | wc -w)
+    initcount=1
+    IFS=","
+    for checkstages in $existone
+    do
+      if [ "$liststages" == "$checkstages" ]
+      then
+        echo "Yes stage matches $liststages = $checkstages"
+        ls | grep $checkstages >> ../list
+        if [ "$?" != "0" ]
+        then
+          echo "The stage name:--> $liststages not found .." 
+        fi
+        filestg=$(ls | grep $checkstages)
+	dynaparm=$(yq -r '.stages.'"$liststages"'' ../../stage-input.yml -o json)
+	if [ "$dynaparm" != "null" ]
+	then
+	  echo It has params
+          jq --argjson param "$dynaparm" '. += $param' $filestg > duplicate.json
+	  rm -rf $filestg
+	  mv duplicate.json $filestg
+        fi
+      else
+        if [ "$loopcount" == "$initcount" ]
+        then
+          echo "The stage name:--> $liststages not found .."
+	  exit 1
+        fi
+        initcount=$((initcount+1))
+      fi
+    done
+  done
+  stageid=
+  refid=1
+  IFS=","
+  while read -r filestg; do
+    echo value is $filestg
+    jq  --argjson stage "$(<$filestg)" '.stages += [$stage]' plain_pipeline_template.json > formulate.json
+    if [[ -z "$stageid" ]]; then
+    jq --argjson refstage '{"refId":"'$refid'","requisiteStageRefIds":['"$stageid"']}' '.stages['"$stageid"'] += $refstage' formulate.json > inter.json
+    else
+    jq --argjson refstage '{"refId":"'$refid'","requisiteStageRefIds":["'"$stageid"'"]}' '.stages['"$stageid"'] += $refstage' formulate.json > inter.json
+    fi
+    if [ $? != 0 ]; then
+      echo "Error occured in YAML processing, please check the logs"
+      exit 1
+    fi
+    rm -rf plain_pipeline_template.json formulate.json final_pipeline.json
+    mv inter.json plain_pipeline_template.json
+    rm -rf inter.json
+    filestg=manualjudgement_stage.json
+    refid=$((refid+1))
+    stageid=$((stageid+1))
+  done < ../list
+  rm -rf ../list
+  rm -rf ../../complete_pipeline.json
+  cp plain_pipeline_template.json ../../complete_pipeline.json
+  echo "Complete Pipeline json"
+  echo "============================================================="
+  cat ../../complete_pipeline.json
+  echo "============================================================="
+  cd ../../
+  rm -rf pipeline-json
+fi
